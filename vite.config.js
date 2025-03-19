1653f352317101043f3b2f9e9e5d1272f570d541
HridayDev <hridaykh1234@gmail.com>
2025-03-19 21:44:36 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/my-react-app/', // Replace with your repository name
+	plugins: [react()],
+	base: './',
+	build: {
+		outDir: 'docs', // Change this to your desired directory
+	}
 })
